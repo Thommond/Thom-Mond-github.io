bd40d82d8d430585aeab3b0fdcf7dc137ac8e30b
Thom Mondeaux <Thommond554@gmail.com>
2020-06-16 20:04:47 -0400

    I deleted unneeded files.
    I updated the index.html to have more content and containers.
    
    I added basic styles so I can see differences.
    
    I added JavaScript to assist in a sticky header creation.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// TODO: Make a minimum amount of javaScript included in the project
+
+// Main function
+window.onscroll = () => {stickHeader()};
+
+// Get the header
+var header = document.getElementById("theHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Adds the sticky header class when offset is not start
+stickHeader = () => {
+
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  }
+  // Sticks to page if offset is not start
+   else {
+    header.classList.remove("sticky");
+  }
+}
