58147dfb43130add59a69df53f6d7685d3a2af56
Thom Mondeaux <Thommond554@gmail.com>
2020-06-24 14:58:17 -0400

    I made the header styles update on scroll.
    I left todos in index.js and index.html to update
    structure and styles for more proffesional header.
    
    I added google fonts like Be vietnam and Raleway.

--- index.js
+++ index.js
@@ -2,24 +2,29 @@
 // Main function
 window.onscroll = () => {stickHeader()};
 
-// Get the header
-var header = document.getElementById("theHeader");
+// Get the header and nav
+let header = document.getElementById("theHeader")
+let nav = document.getElementById("theNav");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+const sticky = header.offsetTop;
 
 // Adds the sticky header class when offset is not start
 
+// TODO: Add so header 'shrinks' in size and styles follow shrink
+// TODO: Delete p element from header and add when user is at top of page
 stickHeader = () => {
 
   if (window.pageYOffset > sticky) {
 
     header.classList.add("sticky");
+    nav.classList.add("stickyTwo");
 }
 // Sticks to page if offset is not start
    else {
 
     header.classList.remove("sticky");
+    nav.classList.remove("stickyTwo");
   }
 
 }
