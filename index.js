e118fab626cbe7e9f2c32bbb319489fe08f01af6
Thom Mondeaux <Thommond554@gmail.com>
2020-06-24 14:08:02 -0400

    I added a few styles for a "sticky" header.
    I updated the HTML to have match the styles.
    
    I added one comment to the index.js page for readability.

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ var header = document.getElementById("theHeader");
 var sticky = header.offsetTop;
 
 // Adds the sticky header class when offset is not start
+
 stickHeader = () => {
 
   if (window.pageYOffset > sticky) {
+
     header.classList.add("sticky");
-  }
-  // Sticks to page if offset is not start
+}
+// Sticks to page if offset is not start
    else {
+
     header.classList.remove("sticky");
   }
+
 }
