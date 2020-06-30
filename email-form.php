293859ad3d472bdfd947ff32e1e04be67a180b87
Thom Mondeaux <Thommond554@gmail.com>
2020-06-30 17:47:16 -0400

    Added higher quality images to the backgrounds.
    Added email-form.php file with php regonizing emails.
    
    Updated index.html to have divs in form and classes for php
    post action.

--- email-form.php
+++ email-form.php
@@ -1,21 +1,8 @@
+
+<html>
+<h1>
 <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$from = 'From: *.Thommondeaux.com';
-$to = 'Thommond@protonmai.com';
-$subject = 'Email from Proffestional Website';
-$body = "From $name\n E-Mail: $email\n Message:\n $message";
 
-
-if ($_POST['submit']) {
-	if ( mail($to, $subject, $body, $from) ) {
-		$successMessage = "Message successfully sent";
-
-	}
-		else {
-			$successMessage = "Message Failed to send, Try again.";
-		}
-
-
-}
 ?>
+</h1>
+</html>
--- index.js
+++ index.js
@@ -41,18 +41,3 @@ let stickHeader = () => {
   }
 
 }
-
-let sendEmail = () => {
-        let name=$('#name').val();
-        let email=$('#email').val();
-        let message=$('#message').val();
-        $.post("send_mail.php",'name='+name+'&email='+email'&message='+message,function(result,status,xhr) {
-                if( status.toLowerCase()=="error".toLowerCase() )
-                { alert("An Error Occurred.."); }
-                else {
-                    //alert(result);
-                    $('#sucessMessage').html(result);
-                }
-            })
-            .fail(function(){ alert("something went wrong. Please try again") });
-    }
